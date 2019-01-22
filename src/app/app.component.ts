a00bc5c46be498a8feef8ba4d9df21b21e05a75d
Alex Garcia, Cole Marsteller and Michael Brown <michael@>
2019-01-22 14:03:09 -0800

    add instanciate object and insert to html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from './models/recipe.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Recipe Box';
+
+  recipes: Recipe[] = [
+    new Recipe('put potatoes in bowl and mash','mashed potatoes',['potatoes','butter', 'milk','salt','pepper']),
+    new Recipe('put steak in bowl and mash','Fillet Mignon',['cow','butcher','nail gun','flamethrower','frenchman'])
+  ];
 }
