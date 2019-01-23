22a700f87b65ffc4b1f286716351bffd62bedac7
Christopher Cahill and Michael Brown <michael@>
2019-01-23 10:12:33 -0800

    indexes recipe ingredients for selectIngredients

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,4 +13,30 @@ export class AppComponent {
     new Recipe('put potatoes in bowl and mash','mashed potatoes',['potatoes','butter', 'milk','salt','pepper']),
     new Recipe('put steak in bowl and mash','Fillet Mignon',['cow','butcher','nail gun','flamethrower','frenchman'])
   ];
+
+  selectedRecipe: Recipe = null;
+
+  selectedIngredient: string = null;
+  selectedIngredientIndex: number = null;
+
+
+  selectRecipe(clickedRecipe){
+    this.selectedRecipe = clickedRecipe;
+  }
+
+  selectIngredient(clickedIngredient, clickedIngredientIndex) {
+    this.selectedIngredient = clickedIngredient;
+    this.selectedIngredientIndex = clickedIngredientIndex
+  }
+
+  finishedEditingIngredient() {
+    this.selectedIngredient = null;
+    this.selectedIngredientIndex = null;
+  }
+
+  finishedEditingRecipe() {
+    this.selectedRecipe = null;
+    this.selectedIngredient = null;
+    this.selectedIngredientIndex = null;
+  }
 }
